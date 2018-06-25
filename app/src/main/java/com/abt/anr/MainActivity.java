722e25b294ea086986e2911fd2ee3563ff45e8c5
huangweiqi <askviky2010@gmail.com>
2018-06-25 12:11:21 +0800

    处理anr问题

--- app/src/main/java/com/abt/anr/MainActivity.java
+++ app/src/main/java/com/abt/anr/MainActivity.java
@@ -1,9 +1,13 @@
 package com.abt.anr;
 
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * activity5秒没有影响触屏或者按键操作，报anr
+ * broadcast10秒没有影响触屏或者按键操作，报anr
+ * service20秒没有影响触屏或者按键操作，报anr
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SystemClock.sleep(8*1000);
+        //SystemClock.sleep(8*1000); // 另种sleep都会导致anr
+        try {
+            Thread.sleep(8*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
